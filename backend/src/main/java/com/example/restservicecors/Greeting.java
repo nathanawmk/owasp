356380b521983d479405cdc3e6e5363cc140668f
Nathan Aw <nathan.mk.aw@gmail.com>
2020-06-12 20:16:36 +0800

    Add files via upload

---
+++ backend/src/main/java/com/example/restservicecors/Greeting.java
@@ -0,0 +1,19 @@
+package com.example.restservicecors;
+
+public class Greeting {
+
+	private final String content;
+
+	public Greeting() {
+		this.content = "";
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+}
---
+++ backend/src/main/java/com/example/restservicecors/GreetingController.java
@@ -0,0 +1,48 @@
+package com.example.restservicecors;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "%s";
+	private final AtomicLong counter = new AtomicLong();
+
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping("/accountdetails")
+	public Greeting greeting(@RequestParam("id") long id) {
+		System.out.println("==== Bank Account ID ====" + id);
+		if (id == 123456789)
+		{
+				return new Greeting(String.format(template, 500));
+		}
+		else if (id == 789456123)
+		{
+			
+				return new Greeting(String.format(template, 20000));
+		}	
+		else if (id == 111111111)
+		{
+			return new Greeting(String.format(template, 100));
+		}
+		else if (id == 222222222)
+		{
+			return new Greeting(String.format(template, 500 ));
+		}
+	return new Greeting(String.format(template, 0));
+	}
+	
+
+
+	@GetMapping("/greeting-javaconfig")
+	public Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
+		System.out.println("==== in greeting ====");
+		return new Greeting(String.format(template, name));
+	}
+
+}
